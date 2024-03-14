9dba67d84cb397a3eb062cd1392d2fa1b19c2ea6
İsmail Harmanda <iharmanda@hotmail.com>
2024-03-14 15:58:25 +0300

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "@/app/ui/global.css"
+import {inter} from "@/app/ui/fonts"
+import clsx from "clsx";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className,"antialiased")}>{children}</body>
     </html>
   );
 }
