611e4a4dc763e2cc8479016798c63b3a5105f62d
İsmail Harmanda <iharmanda@hotmail.com>
2024-03-16 03:36:16 +0300

    handle queries and implement search feature

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,33 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams()
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string)=> {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  },400)
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+      defaultValue={searchParams.get('query')?.toString()}
+      onChange={(e) => handleSearch(e.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
